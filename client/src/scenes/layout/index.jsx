0ea1f11ffcf80bc0f2733b47244fb5cba1be2c9a
Exolan <97833724+Exolan@users.noreply.github.com>
2023-04-19 11:24:08 +0300

    Add files via upload

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { Box, useMediaQuery } from "@mui/material";
+import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+import Navbar from "components/Navbar";
+import Sliderbar from "components/Sliderbar";
 
 function Layout() {
-  return <div>Layout</div>;
+  const isNonMobile = useMediaQuery("(min-width: 600px");
+  const [isSliderbarOpen, setIsSliderbarOpen] = useState(true);
+  return (
+    <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
+      <Sliderbar
+        isNonMobile={isNonMobile}
+        drawerWidth="250px"
+        isSliderbarOpen={isSliderbarOpen}
+        setIsSliderbarOpen={setIsSliderbarOpen}
+      />
+      <Box>
+        <Navbar
+          isSliderbarOpen={isSliderbarOpen}
+          setIsSliderbarOpen={setIsSliderbarOpen}
+        />
+        <Outlet />
+      </Box>
+    </Box>
+  );
 }
 
 export default Layout;
